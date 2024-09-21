d55a7839d78690074cec3c9b23dab88a687adcf5
aliffarhanayyasihasan <ayyasifarhan@gmail.com>
2024-09-21 15:15:19 +0700

    latihan coding 21 september 2024 bismillah calon programer masadepan

---
+++ main.py
@@ -0,0 +1,13 @@
+nilai = int(input("masukkan nilai: "))
+              
+if (nilai > 80):
+    print("selamat anda lulus, dengan nilai yang memuaskan")
+elif (nilai > 90 and nilai < 100):
+    print("selamat anda lulus, dengan nilai yang memuaskan")
+elif (nilai > 70):
+    print("lulus")
+else:
+    print("anda masuh belum lulus tes, semangat dan cobba laggi")
+
+
+# for loop
